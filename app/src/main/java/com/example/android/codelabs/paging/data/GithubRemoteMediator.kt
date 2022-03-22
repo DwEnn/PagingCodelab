314309a51024113616bba6ec82cd215a1fa6faa5
DwEnn <prgohn@gmail.com>
2022-03-22 22:19:52 +0900

    18. LoadType 을 기반으로 페이지 가져오기

--- app/src/main/java/com/example/android/codelabs/paging/data/GithubRemoteMediator.kt
+++ app/src/main/java/com/example/android/codelabs/paging/data/GithubRemoteMediator.kt
@@ -24,13 +24,34 @@ class GithubRemoteMediator(
     override suspend fun load(loadType: LoadType, state: PagingState<Int, Repo>): MediatorResult {
         val page = when (loadType) {
             LoadType.PREPEND -> {
-                TODO()
+                val remoteKeys = getRemoteKeyForFirstItem(state)
+                // If remoteKeys is null, that means the refresh result is not in the database yet.
+                // We can return Success with endOfPaginationReached = false because Paging
+                // will call this method again if RemoteKeys becomes non-null.
+                // If remoteKeys is NOT NULL but its prevKey is null, that means we've reached
+                // the end of pagination for append.
+                val prevKey = remoteKeys?.prevKey
+                    ?: return MediatorResult.Success(
+                        endOfPaginationReached = remoteKeys != null
+                    )
+                prevKey
             }
             LoadType.REFRESH -> {
-                TODO()
+                val remoteKeys = getRemoteKeyClosestToCurrentPosition(state)
+                remoteKeys?.nextKey?.minus(1) ?: GITHUB_STARTING_PAGE_INDEX
             }
             LoadType.APPEND -> {
-                TODO()
+                val remoteKeys = getRemoteKeyForLastItem(state)
+                // If remoteKeys is null, that means the refresh result is not in the database yet.
+                // We can return Success with endOfPaginationReached = false because Paging
+                // will call this method again if RemoteKeys becomes non-null.
+                // If remoteKeys is NOT NULL but its prevKey is null, that means we've reached
+                // the end of pagination for append.
+                val nextKey = remoteKeys?.nextKey
+                    ?: return MediatorResult.Success(
+                        endOfPaginationReached = remoteKeys != null
+                    )
+                nextKey
             }
         }
 
@@ -64,6 +85,38 @@ class GithubRemoteMediator(
             return MediatorResult.Error(e)
         } catch (e: HttpException) {
             return MediatorResult.Error(e)
+        }
+    }
+
+    private suspend fun getRemoteKeyForLastItem(state: PagingState<Int, Repo>): RemoteKeys? {
+        // Get the last page that was retrieved, that contained items.
+        // From that last page, get the last item
+        return state.pages.lastOrNull {
+            it.data.isNotEmpty()
+        }?.data?.lastOrNull()?.let { repo ->
+            // Get the remote keys fof the last item retrieved
+            repoDatabase.remoteKeyDao().remoteKeysRepoId(repo.id)
+        }
+    }
+
+    private suspend fun getRemoteKeyForFirstItem(state: PagingState<Int, Repo>): RemoteKeys? {
+        // Get the first page that was retrieved, that contained items.
+        // From that first page, get the first item
+        return state.pages.firstOrNull {
+            it.data.isNotEmpty()
+        }?.data?.firstOrNull()?.let {
+            // Get the remote keys of the first items retrieved
+            repoDatabase.remoteKeyDao().remoteKeysRepoId(it.id)
+        }
+    }
+
+    private suspend fun getRemoteKeyClosestToCurrentPosition(state: PagingState<Int, Repo>): RemoteKeys? {
+        // The paging library is trying to load data after the anchor position
+        // Get the item closest to the anchor position
+        return state.anchorPosition?.let { position ->
+            state.closestItemToPosition(position)?.id?.let { repoId ->
+                repoDatabase.remoteKeyDao().remoteKeysRepoId(repoId)
+            }
         }
     }
 }
